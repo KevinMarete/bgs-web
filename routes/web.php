6143ba17b08d23cd9f0058b2bb34437a0632ae81
KevinMarete <kevomarete@gmail.com>
2020-02-10 19:00:45 +0300

    Added routes for accessing various auth views such as sign-in, sign-up, registration

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,45 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.sign-in');
+});
+
+Route::get('/registration', function () {
+    return view('auth.registration');
+});
+
+Route::get('/create-organization', function () {
+    return view('auth.create-organization');
+});
+
+Route::get('/sign-up', function () {
+    return view('auth.sign-up');
+});
+
+Route::get('/sign-in', function () {
+    return view('auth.sign-in');
+});
+
+Route::get('/forgot-password', function () {
+    return view('auth.forgot-password');
+});
+
+Route::get('/dashboard', function () {
+    $data = array (
+        'page_title' => 'Dashboard',
+        'content_view' => View::make('account.profile')
+    );
+    return view('template.template', $data);
+});
+
+Route::get('/profile', function () {
+    return view('account.profile');
+});
+
+Route::get('/change-password', function () {
+    return view('account.change-password');
+});
+
+Route::get('/manage-subscription', function () {
+    return view('account.manage-subscription');
 });

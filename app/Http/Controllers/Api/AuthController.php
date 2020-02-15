bc219aa36b9c02a84658539d3febf3e932e510cb
KevinMarete <kevomarete@gmail.com>
2020-02-15 22:20:20 +0300

    Added API Controllers

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ActivateAccountEmail;
+use App\Mail\ForgotPasswordEmail;
+use Illuminate\Support\Str;
+
+class AuthController extends Controller
+{	
+	protected $seeder = 'BGSSeeder';
+
+    public function unauthorized(){
+		return response(['error'=>'Not authorized'], 401);
+    }
+    
+	public function register(Request $request) 
+	{
+		$this->validate($request, User::$rules);
+	    $request['password']=Hash::make($request['password']);
+	    $user = User::create($request->toArray());
+
+	    return response($user, 200);
+	}
+
+	public function activateaccountemail(Request $request){
+		$user = User::find($request->id);
+		if ($user) {
+			$user->code = strtoupper(substr(md5($user->id.$user->email.$this->seeder), 0, 4));
+			Mail::send(new ActivateAccountEmail($user));
+			return response(['msg'=> 'Mail sent'], 200);
+		} else {
+			$response = 'User does not exist';
+	        return response(['error' => $response], 401);
+	    }
+	}
+
+	public function activate(Request $request) 
+	{	
+	    $user = User::find($request->id);
+		if ($user) {
+			$code = strtoupper(substr(md5($user->id.$user->email.$this->seeder), 0, 4));
+			if($code == $request->code){
+				if(empty($user->email_verified_at)){
+	        		$user->email_verified_at = date('Y-m-d H:i:s');
+	        		$user->save();
+	        	}
+				return response(['msg'=> 'Account verified'], 200);
+			}else{
+				$response = 'Account not verified';
+	        	return response(['error' => $response], 401);
+			}
+		} else {
+			$response = 'User does not exist';
+	        return response(['error' => $response], 401);
+	    }
+	}
+
+	public function login(Request $request) 
+	{	
+	    $user = User::where('email', $request->email)->first();
+	    if ($user) {
+	        if (Hash::check($request->password, $user->password)) {
+	        	if(empty($user->email_verified_at)){
+	        		$response = "Email unverified";
+	            	return response(['error' => $response, 'user_id' => $user->id]);
+	        	}
+	            $token = $user->createToken($this->seeder)->accessToken;
+	            $response = ['token' => $token];
+	            return response($response, 200);
+	        } else {
+	            $response = "Password missmatch";
+	            return response(['error' => $response]);
+	        }
+	    } else {
+	        $response = 'User does not exist';
+	        return response(['error' => $response]);
+	    }
+    }
+    
+	public function forgotpasswordemail(Request $request){
+		$user = User::where('email', $request->email)->first();
+	    if ($user) {
+			//Change Old Password
+			$new_password = Str::random(8);
+			$user->password = Hash::make($new_password);
+			$user->save();
+			//Send new_password to email
+			$user->password = $new_password;
+			Mail::send(new ForgotPasswordEmail($user));
+			return response(['msg'=> 'Password was reset and sent to your email!'], 200);
+	    } else {
+	        $response = 'User does not exist';
+	        return response(['error' => $response], 401);
+	    }
+    }
+    
+	public function changepassword(Request $request){
+		$user = User::where('email', $request->email)->first();
+	    if ($user) {
+	        if (Hash::check($request->password, $user->password)) {
+	        	if($request->new_password == $request->confirm_password){
+			        $user->password=Hash::make($request['new_password']);
+			        $user->save();
+			        $response = 'Password changed';
+	        		return response(['msg' => $response], 200);
+			    } else {
+			    	$response = 'Passwords do not match';
+		        	return response(['error' => $response], 422);
+			    }
+	        } else {
+	            $response = "Wrong username or password";
+	            return response(['error' => $response], 401);
+	        }
+	    } else {
+	        $response = 'User does not exist';
+	        return response(['error' => $response], 401);
+	    }
+	}
+
+	public function viewprofile(Request $request){
+		$user = User::with('organization')->find(auth('api')->user()->id);
+	    return response($user, 200);
+	}
+
+	public function updateprofile(Request $request){
+		$user = User::find($request->id);
+		if(!empty($user)){
+			$email_exists = User::where('email', $request->email)->where('id', '!=', $user->id)->first();
+			if(empty($email_exists)){
+				$user->firstname = $request->firstname;
+				$user->lastname = $request->lastname;
+				$user->phone = $request->phone;
+				$user->email = $request->email;
+				$user->save();
+				$response = 'Profile updated';
+	        	return response(['msg' => $response], 200);
+	        } else {
+	        	$response = 'Email exists';
+	        	return response(['error' => $response], 209);
+	        }
+		}
+		return response(['error' => 'Details could not be updated'], 500);
+	}
+
+	public function logout (Request $request) {
+	    $token = $request->user()->token();
+	    $token->revoke();
+
+	    $response = 'You have been succesfully logged out!';
+	    return response(['msg' => $response], 200);
+    }
+
+}
---
+++ app/Http/Controllers/Api/OrganizationController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Organization;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class OrganizationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $organizations = Organization::all();
+        return response()->json($organizations);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, Organization::$rules);
+        $organization = Organization::create($request->all());
+        return response()->json($organization);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $organization = Organization::find($id);
+        if(is_null($organization)){
+            return response()->json(['error' => 'not_found']);
+        }
+        return response()->json($organization);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, Organization::$rules);
+        $organization  = Organization::find($id);
+        if(is_null($organization)){
+            return response()->json(['error' => 'not_found']);
+        }
+        $organization->update($request->all());
+        return response()->json($organization);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $organization = Organization::find($id);
+        if(is_null($organization)){
+            return response()->json(['error' => 'not_found']);
+        }
+        $organization->delete();
+        return response()->json(['msg' => 'Removed successfully']);
+    }
+}

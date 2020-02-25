019598385a47776befd902ea4d1e4ad09513b43a
KevinMarete <kevomarete@gmail.com>
2020-02-25 22:17:26 +0300

    Added new seller views

---
+++ resources/views/seller/table.blade.php
@@ -0,0 +1,58 @@
+<div class="sb-page-header pb-10 sb-page-header-dark bg-gradient-primary-to-secondary">
+    <div class="container-fluid">
+        <div class="sb-page-header-content py-5">
+            <h1 class="sb-page-header-title">
+                <div class="sb-page-header-icon"><i data-feather="activity"></i></div>
+                <span>{{ $resource_name }} </span>
+            </h1>
+        </div>
+    </div>
+</div>
+<div class="container-fluid mt-n10">
+    <div class="card mb-4">
+        <div class="card-header">{{ $resource_name }} Listing</div>
+        <div class="card-body">
+            <div class="sb-datatable table-responsive">
+                <table class="table table-bordered table-hover" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            @foreach ($table_headers as $key => $header)
+                                <th>{{ ucwords($header) }}</th>  
+                            @endforeach
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tfoot>
+                        <tr>
+                            @foreach ($table_headers as $header)
+                                <th>{{ ucwords($header) }}</th>  
+                            @endforeach
+                            <th>Actions</th>
+                        </tr>
+                    </tfoot>
+                    <tbody>
+                        @foreach ($table_data as $row)
+                            <tr>
+                            @foreach ($table_headers as $header)
+                                @if (is_array($row[$header]))
+                                    <td>{{ $row[$header][(array_key_exists('name', $row[$header]) ? 'name' : 'molecular_name')] }}</td>
+                                @else 
+                                    <td>{{ $row[$header] }}</td>                                                  
+                                @endif
+                            @endforeach
+                                <td>
+                                    <button class="btn sb-btn-datatable sb-btn-icon sb-btn-transparent-dark mr-2">
+                                        <i data-feather="more-vertical"></i>
+                                    </button>
+                                    <button class="btn sb-btn-datatable sb-btn-icon sb-btn-transparent-dark">
+                                        <i data-feather="trash-2"></i>
+                                    </button>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>

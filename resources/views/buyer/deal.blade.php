4abd6cbda1cd4611f15a85211109aba07ceda464
KevinMarete <kevomarete@gmail.com>
2020-02-25 22:17:46 +0300

    Added new buyer views

---
+++ resources/views/buyer/deal.blade.php
@@ -0,0 +1,18 @@
+<div class="sb-page-header pb-10 sb-page-header-dark bg-gradient-primary-to-secondary">
+    <div class="container-fluid">
+        <div class="sb-page-header-content py-5">
+            <h1 class="sb-page-header-title">
+                <div class="sb-page-header-icon"><i data-feather="activity"></i></div>
+                <span>Deals</span>
+            </h1>
+        </div>
+    </div>
+</div>
+<div class="container-fluid mt-n10">
+    <div class="card mb-4">
+        <div class="card-header"> </div>
+        <div class="card-body">
+            
+        </div>
+    </div>
+</div>

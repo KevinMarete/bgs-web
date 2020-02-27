c59717cbc4938c4c2b834f66773cf705068ec2a3
KevinMarete <kevomarete@gmail.com>
2020-02-27 22:40:41 +0300

    Added manage links for add, edit and delete in admin table list

--- resources/views/admin/table.blade.php
+++ resources/views/admin/table.blade.php
@@ -9,8 +9,14 @@
     </div>
 </div>
 <div class="container-fluid mt-n10">
+    @if (Session::has('bgs_msg'))
+        {!! session('bgs_msg') !!}
+    @endif
     <div class="card mb-4">
-        <div class="card-header">{{ $resource_name }} Listing</div>
+        <div class="card-header">
+            {{ $resource_name }} Listing
+            <a href="/manage/{{ str_replace(' ', '-', strtolower($resource_name)) }}" class="btn btn-primary ml-auto"><i data-feather="plus"></i> Add {{ $resource_name }}</a>
+        </div>
         <div class="card-body">
             <div class="sb-datatable table-responsive">
                 <table class="table table-bordered table-hover" id="dataTable" width="100%" cellspacing="0">
@@ -35,18 +41,18 @@
                             <tr>
                             @foreach ($table_headers as $header)
                                 @if (is_array($row[$header]))
-                                    <td>{{ $row[$header]['name'] }}</td>
+                                    <td>{{ $row[$header][(array_key_exists('name', $row[$header]) ? 'name' : 'molecular_name')] }}</td>
                                 @else 
                                     <td>{{ $row[$header] }}</td>                                                  
                                 @endif
                             @endforeach
                                 <td>
-                                    <button class="btn sb-btn-datatable sb-btn-icon sb-btn-transparent-dark mr-2">
+                                    <a href="/manage/{{ str_replace(' ', '-', strtolower($resource_name)) }}/edit/{{ $row['id'] }}" class="btn sb-btn-datatable sb-btn-icon sb-btn-transparent-dark mr-2">
                                         <i data-feather="more-vertical"></i>
-                                    </button>
-                                    <button class="btn sb-btn-datatable sb-btn-icon sb-btn-transparent-dark">
+                                    </a>
+                                    <a href="/manage/{{ str_replace(' ', '-', strtolower($resource_name)) }}/delete/{{ $row['id'] }}" class="delete btn sb-btn-datatable sb-btn-icon sb-btn-transparent-dark">
                                         <i data-feather="trash-2"></i>
-                                    </button>
+                                    </a>
                                 </td>
                             </tr>
                         @endforeach

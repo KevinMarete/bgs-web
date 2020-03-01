cebee27e1489329a22928f72b516c28797808733
KevinMarete <kevomarete@gmail.com>
2020-03-02 00:27:10 +0300

    Added Logic to support managing stocks and bin_card

--- app/Http/Controllers/Seller/SellerController.php
+++ app/Http/Controllers/Seller/SellerController.php
@@ -44,6 +44,27 @@ class SellerController extends MyController
         return view('template.main', $data);
     }
 
+    public function displayBalancesTableView(Request $request)
+    {   
+        $resource = 'stockbalances';
+        $resource_name = ucwords(str_replace('-', ' ', $resource));
+        $token = session()->get('token');
+        $role_id = session()->get('organization.organization_type.role_id');
+        $organization_id = session()->get('organization_id');
+        $view_data = [
+            'resource_name' => $resource_name,
+            'table_headers' => $this->getResourceKeys($resource),
+            'table_data' => $this->getResourceData($token, 'organization/'.$organization_id.'/'.$resource)
+        ];
+        $data = [
+            'page_title' => $resource_name, 
+            'content_view' => View::make('seller.balances_table', $view_data),
+            'menus' => $this->getRoleMenus($token, $role_id),
+        ];
+
+        return view('template.main', $data);
+    }
+
     public function getResourceData($token=null, $resource=null)
     {   
         $resource_data = [];
@@ -66,7 +87,7 @@ class SellerController extends MyController
         if($resource != null){
             $headers = [
                 'offers' => ['id', 'description', 'valid_from', 'valid_until', 'discount', 'max_discount_amount', 'organization'],
-                'stocks' => ['id', 'product', 'stock_type', 'transaction_date', 'batch_number', 'expiry_date', 'quantity', 'balance', 'organization']
+                'stockbalances' => ['molecular_name', 'brand_name', 'pack_size', 'balance']
             ];
             $header_data = $headers[$resource];
         }
@@ -131,7 +152,7 @@ class SellerController extends MyController
         $dropdown_data = [];
         $data_sources = [
             'offers' => ['organizations'],
-            'stocks' => ['products', 'stocktypes', 'organizations']
+            'stockbalances' => ['products', 'stocktypes']
         ];
 
         if ($token !== null && $resource !== null){   
@@ -155,5 +176,132 @@ class SellerController extends MyController
         $response = json_decode($response->getBody(), true);
 
         return $response;
+    }
+
+    public function displayTransactionView(Request $request)
+    {   
+        $resource_name = 'stockbalances';
+        $token = session()->get('token');
+        $role_id = session()->get('organization.organization_type.role_id');
+        $view_data = $this->getDropDownData($token, $resource_name);
+        $data = [
+            'page_title' => 'Stocks', 
+            'menus' => $this->getRoleMenus($token, $role_id),
+            'content_view' => View::make('seller.transactions', $view_data)
+        ];
+
+        return view('template.main', $data);
+    }
+
+    public function displayBinCardView(Request $request)
+    {   
+        $token = session()->get('token');
+        $role_id = session()->get('organization.organization_type.role_id');
+        $organization_id = session()->get('organization_id');
+        $product_id = $request->product;
+        $view_data = [
+            'product' => $this->getResourceData($token, 'product/'.$product_id),
+            'stocks' => $this->getResourceData($token, 'organization/'.$organization_id.'/stocks/'.$product_id),
+            'balances' => $this->getResourceData($token, 'organization/'.$organization_id.'/stockbalances/'.$product_id)
+        ];
+        $data = [
+            'page_title' => 'Stocks', 
+            'menus' => $this->getRoleMenus($token, $role_id),
+            'content_view' => View::make('seller.bincard', $view_data)
+        ];
+
+        return view('template.main', $data);
+    }
+
+    public function saveTransactions(Request $request)
+    {   
+        $post_data = $request->all();
+        $organization_id = session()->get('organization_id');
+        $user_id = session()->get('id');
+        $errors = 0;
+
+        foreach($post_data['product_id'] as $key=> $product_id)
+        {   
+            $batch_number = $post_data['batch_number'][$key];
+            $expiry_date = $post_data['expiry_date'][$key];
+            $quantity = $post_data['quantity'][$key];
+            $stock_type_id = $post_data['stock_type_id'];
+
+            //Get balance
+            $balance_data = $this->getExpectedBalance([
+                'batch_number' => $batch_number,
+                'expiry_date' => $expiry_date,
+                'quantity' => $quantity,
+                'product_id' => $product_id,
+                'stock_type_id' => $stock_type_id,
+                'organization_id' => $organization_id
+            ]);
+
+            //Build request object
+            $request_data = [
+                'transaction_date' => $post_data['transaction_date'],
+                'batch_number' => $batch_number,
+                'expiry_date' => $expiry_date,
+                'quantity' => $balance_data['quantity'],
+                'balance' => $balance_data['closing_balance'],
+                'product_id' => $product_id,
+                'stock_type_id' => $stock_type_id,
+                'organization_id' => $organization_id,
+                'user_id' => $user_id,
+            ];
+            
+            //Send request data to Api
+            $response = $this->client->post("stock", [
+                'headers' => [
+                    'Authorization' => 'Bearer '.session()->get('token')
+                ],
+                'json' => $request_data
+            ]);
+            
+            $response = json_decode($response->getBody(), true);
+
+            //Check success
+            if(isset($response['error'])){
+                $errors += 1;
+            }
+        }
+
+        if($errors > 0){
+            $flash_msg = '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                <strong>Error!</strong> '.$errors.' transactions were not added successfully
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>';
+        }else{
+            $flash_msg = '<div class="alert alert-success alert-dismissible fade show" role="alert">
+                            <strong>Success!</strong> Your transactions were added successfully
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>';
+        }
+
+        $request->session()->flash('bgs_msg', $flash_msg);
+
+        return redirect('/stocks');
+    }
+
+    public function getExpectedBalance($request_data=[])
+    {   
+        $response_data = [];
+        if(!empty($request_data))
+        {
+            //Send request data to Api
+            $response = $this->client->post("calculatebalance", [
+                'headers' => [
+                    'Authorization' => 'Bearer '.session()->get('token')
+                ],
+                'json' => $request_data
+            ]);
+            
+            $response_data = json_decode($response->getBody(), true);   
+        }
+        return $response_data;
     }
 }

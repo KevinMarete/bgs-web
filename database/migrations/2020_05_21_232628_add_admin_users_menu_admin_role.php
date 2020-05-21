fdfd65dd15bac2497f78d536ec2ee3afb0adfd3f
KevinMarete <kevomarete@gmail.com>
2020-05-22 00:25:44 +0300

    feat: Assigned Admin Users menu to Admin role

---
+++ database/migrations/2020_05_21_232628_add_admin_users_menu_admin_role.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class AddAdminUsersMenuAdminRole extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $menu = DB::table('tbl_menu')->where('name', 'Admin Users')->first();
+        $role = DB::table('tbl_role')->where('name', 'admin')->first();
+
+        DB::table('tbl_menu_role')->insert(
+            ['menu_id' => $menu->id, 'role_id' => $role->id, 'created_at' => now()]
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $menu = DB::table('tbl_menu')->where('name', 'Admin Users')->first();
+        $role = DB::table('tbl_role')->where('name', 'admin')->first();
+
+        DB::table('tbl_menu_role')->where('menu_id', $menu->id)->where('role_id', $role->id)->delete();
+    }
+}

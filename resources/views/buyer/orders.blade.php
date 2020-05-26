677d1821c6287b48b9cdc2142d7c464fbf6a9a8f
KevinMarete <kevomarete@gmail.com>
2020-05-26 12:52:42 +0300

    fix: added card-header to order listing

--- resources/views/buyer/orders.blade.php
+++ resources/views/buyer/orders.blade.php
@@ -7,9 +7,10 @@
             </h1>
         </div>
     </div>
+</div>
 <div class="container-fluid mt-n10">
     @if (Session::has('bgs_msg'))
-        {!! session('bgs_msg') !!}
+    {!! session('bgs_msg') !!}
     @endif
     <div class="card mb-4">
         <div class="card-header">
@@ -21,7 +22,7 @@
                     <thead>
                         <tr>
                             @foreach ($table_headers as $key => $header)
-                                <th>{{ ucwords($header) }}</th>  
+                            <th>{{ ucwords($header) }}</th>
                             @endforeach
                             <th>Actions</th>
                         </tr>
@@ -29,29 +30,29 @@
                     <tfoot>
                         <tr>
                             @foreach ($table_headers as $header)
-                                <th>{{ ucwords($header) }}</th>  
+                            <th>{{ ucwords($header) }}</th>
                             @endforeach
                             <th>Actions</th>
                         </tr>
                     </tfoot>
                     <tbody>
                         @foreach ($table_data as $row)
-                            <tr>
-                                <td>{{ $row['id'] }}</td>
-                                @if($role_name !== 'buyer')
-                                <td>{{ $row['organization']['name'] }}</td>
-                                @endif
-                                <td>{{ $row['status'] }}</td>
-                                <td>{{ $row['created_at'] }}</td>
-                                <td>KES {{ number_format($row['product_total']) }}</td>
-                                <td>KES {{ number_format($row['shipping_total']) }}</td>
-                                <td>KES {{ number_format($row['product_total'] + $row['shipping_total']) }}</td>
-                                <td>
-                                    <a href="/view-order/{{ $row['id'] }}" class="btn sb-btn-datatable sb-btn-icon sb-btn-transparent-dark mr-2">
-                                        <i data-feather="more-vertical"></i>
-                                    </a>
-                                </td>
-                            </tr>
+                        <tr>
+                            <td>{{ $row['id'] }}</td>
+                            @if($role_name !== 'buyer')
+                            <td>{{ $row['organization']['name'] }}</td>
+                            @endif
+                            <td>{{ $row['status'] }}</td>
+                            <td>{{ $row['created_at'] }}</td>
+                            <td>KES {{ number_format($row['product_total']) }}</td>
+                            <td>KES {{ number_format($row['shipping_total']) }}</td>
+                            <td>KES {{ number_format($row['product_total'] + $row['shipping_total']) }}</td>
+                            <td>
+                                <a href="/view-order/{{ $row['id'] }}" class="btn sb-btn-datatable sb-btn-icon sb-btn-transparent-dark mr-2">
+                                    <i data-feather="more-vertical"></i>
+                                </a>
+                            </td>
+                        </tr>
                         @endforeach
                     </tbody>
                 </table>

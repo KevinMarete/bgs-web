45f90bd9f81657e95a33e7b518214aa37efc87f9
KevinMarete <kevomarete@gmail.com>
2020-06-04 12:17:10 +0300

    feat: Added pricelist csv template download with columns brand_name, molecular_name, pack_size, unit_price and available_stock

--- resources/views/seller/pricelist/import.blade.php
+++ resources/views/seller/pricelist/import.blade.php
@@ -12,6 +12,48 @@
   <div class="card mb-4">
     <div class="card-header"> </div>
     <div class="card-body">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-sm-12 col-md-6 col-lg-8">
+            <div class="card">
+              <div class="card-header bg-primary text-white"><i class="fa fa-upload"></i> &nbsp; Import Template
+              </div>
+              <div class="card-body">
+                <form>
+                  <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" class="form-control" id="name" aria-describedby="emailHelp" placeholder="Enter name" required>
+                  </div>
+                  <div class="form-group">
+                    <label for="email">Email address</label>
+                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email" required>
+                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                  </div>
+                  <div class="form-group">
+                    <label for="message">Message</label>
+                    <textarea class="form-control" id="message" rows="6" required></textarea>
+                  </div>
+                  <div class="mx-auto">
+                    <button type="submit" class="btn btn-primary text-right">Submit</button></div>
+                </form>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md-6 col-lg-4">
+            <div class="card bg-light mb-3">
+              <div class="card-header bg-primary text-white"><i class="fa fa-download"></i> &nbsp; Download Template</div>
+              <div class="card-body">
+                <p>
+                  <i class="fa fa-file-csv"></i>
+                  <a href="/assets/files/pricelist_template.csv" download>
+                    Pricelist Template
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </div>

294cb3441eb2fe5046dc3a003ac3707c1ffe55dd
KevinMarete <kevomarete@gmail.com>
2020-06-10 18:29:31 +0300

    feat: added migration to remove promos menu from buyer role

---
+++ database/migrations/2020_06_10_182448_remove_promos_menu_buyer_role.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RemovePromosMenuBuyerRole extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $menu = DB::table('tbl_menu')->where('name', 'Promos')->first();
+        $role = DB::table('tbl_role')->where('name', 'buyer')->first();
+
+        DB::table('tbl_menu_role')->where('menu_id', $menu->id)->where('role_id', $role->id)->delete();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $menu = DB::table('tbl_menu')->where('name', 'Promos')->first();
+        $role = DB::table('tbl_role')->where('name', 'buyer')->first();
+
+        DB::table('tbl_menu_role')->insert(
+            ['menu_id' => $menu->id, 'role_id' => $role->id, 'created_at' => now()]
+        );
+    }
+}

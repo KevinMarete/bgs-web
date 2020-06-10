139376127e86a69669a856d9bd0c7512a444600e
KevinMarete <kevomarete@gmail.com>
2020-06-10 22:45:34 +0300

    feat: added migration to add promotions table

---
+++ database/migrations/2020_06_10_192726_create_promotions_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePromotionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_promotion', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('status');
+            $table->date('display_date');
+            $table->string('display_url');
+            $table->integer('product_now_id')->unsigned();
+            $table->integer('organization_id')->unsigned();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->unique(['status', 'display_date', 'product_now_id', 'organization_id']);
+
+            $table->foreign('product_now_id')->references('id')->on('tbl_product_now')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('organization_id')->references('id')->on('tbl_organization')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbl_promotion');
+    }
+}

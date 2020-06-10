ea8d9a2bf21d4bc24b2a83eb9e5e8364d01c9ce3
KevinMarete <kevomarete@gmail.com>
2020-06-11 00:46:24 +0300

    feat: added two tabs namely slider and static in My Promotions

--- resources/views/seller/promotions/listing.blade.php
+++ resources/views/seller/promotions/listing.blade.php
@@ -13,55 +13,115 @@
   {!! session('bgs_msg') !!}
   @endif
   <div class="card mb-4">
-    <div class="card-header">
-      Promotion Listing
-      <a href="/promotions/new" class="btn btn-primary ml-auto">
-        <i data-feather="plus"></i>
-        Add Promotion Items
-      </a>
-    </div>
+    <div class="card-header"> </div>
     <div class="card-body">
-      <div class="sb-datatable table-responsive">
-        <table class="table table-bordered table-hover dataTable" width="100%" cellspacing="0">
-          <thead>
-            <tr>
-              @foreach ($table_headers as $header)
-              <th>{{ ucwords($header) }}</th>
-              @endforeach
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tfoot>
-            <tr>
-              @foreach ($table_headers as $header)
-              <th>{{ ucwords($header) }}</th>
-              @endforeach
-              <th>Actions</th>
-            </tr>
-          </tfoot>
-          <tbody>
-            @foreach ($table_data as $row)
-            <tr>
-              <td>{{ $row['id'] }}</td>
-              <td>{{ $row['product_now']['product']['brand_name'] }}</td>
-              <td>{{ $row['product_now']['product']['molecular_name'] }}</td>
-              <td>{{ $row['coupon_code'] }}</td>
-              <td>{{ $row['product_now']['unit_price'] }}</td>
-              <td>{{ $row['offer']['discount'] }}</td>
-              <td>{{ $row['offer']['max_discount_amount'] }}</td>
-              <td>
-                <a href="/promotions/edit/{{ $row['id'] }}" class="btn sb-btn-datatable sb-btn-icon sb-btn-transparent-dark mr-2">
-                  <i data-feather="more-vertical"></i>
+      <div class="col-lg-12">
+        <ul class="nav nav-tabs">
+          <li class="nav-item">
+            <a href="" data-target="#slider" data-toggle="tab" class="nav-link active">Slider</a>
+          </li>
+          <li class="nav-item">
+            <a href="" data-target="#static" data-toggle="tab" class="nav-link">Static</a>
+          </li>
+        </ul>
+        <div class="tab-content py-4">
+          <div class="tab-pane active" id="slider">
+            <div class="card mb-4">
+              <div class="card-header">
+                Slider Promotion Listing
+                <a href="/promotions/new" class="btn btn-primary ml-auto">
+                  <i data-feather="plus"></i>
+                  Add Slider Promotion
                 </a>
-                <a href="/promotions/delete/{{ $row['id'] }}" class="delete btn sb-btn-datatable sb-btn-icon sb-btn-transparent-dark">
-                  <i data-feather="trash-2"></i>
+              </div>
+              <div class="card-body">
+                <div class="sb-datatable table-responsive">
+                  <table class="table table-bordered table-hover dataTable" width="100%" cellspacing="0">
+                    <thead>
+                      <tr>
+                        @foreach ($slider['table_headers'] as $header)
+                        <th>{{ ucwords($header) }}</th>
+                        @endforeach
+                        <th>Actions</th>
+                      </tr>
+                    </thead>
+                    <tfoot>
+                      <tr>
+                        @foreach ($slider['table_headers'] as $header)
+                        <th>{{ ucwords($header) }}</th>
+                        @endforeach
+                        <th>Actions</th>
+                      </tr>
+                    </tfoot>
+                    <tbody>
+                      @foreach ($slider['table_data'] as $row)
+                      <tr>
+                        <td>{{ $row['id'] }}</td>
+                        <td>{{ $row['status'] }}</td>
+                        <td>{{ $row['display_date'] }}</td>
+                        <td>{{ $row['product_now']['product']['brand_name'] }}</td>
+                        <td>
+                          <a href="/promotions/edit/{{ $row['id'] }}" class="btn sb-btn-datatable sb-btn-icon sb-btn-transparent-dark mr-2">
+                            <i data-feather="more-vertical"></i>
+                          </a>
+                        </td>
+                      </tr>
+                      @endforeach
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="tab-pane" id="static">
+            <div class="card mb-4">
+              <div class="card-header">
+                Static Promotion Listing
+                <a href="/promotions/new" class="btn btn-primary ml-auto">
+                  <i data-feather="plus"></i>
+                  Add Static Promotion
                 </a>
-              </td>
-            </tr>
-            @endforeach
-          </tbody>
-        </table>
+              </div>
+              <div class="card-body">
+                <div class="sb-datatable table-responsive">
+                  <table class="table table-bordered table-hover dataTable" width="100%" cellspacing="0">
+                    <thead>
+                      <tr>
+                        @foreach ($static['table_headers'] as $header)
+                        <th>{{ ucwords($header) }}</th>
+                        @endforeach
+                        <th>Actions</th>
+                      </tr>
+                    </thead>
+                    <tfoot>
+                      <tr>
+                        @foreach ($static['table_headers'] as $header)
+                        <th>{{ ucwords($header) }}</th>
+                        @endforeach
+                        <th>Actions</th>
+                      </tr>
+                    </tfoot>
+                    <tbody>
+                      @foreach ($static['table_data'] as $row)
+                      <tr>
+                        <td>{{ $row['id'] }}</td>
+                        <td>{{ $row['status'] }}</td>
+                        <td>{{ $row['display_date'] }}</td>
+                        <td>{{ $row['product_now']['product']['brand_name'] }}</td>
+                        <td>
+                          <a href="/promotions/edit/{{ $row['id'] }}" class="btn sb-btn-datatable sb-btn-icon sb-btn-transparent-dark mr-2">
+                            <i data-feather="more-vertical"></i>
+                          </a>
+                        </td>
+                      </tr>
+                      @endforeach
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
-</div>

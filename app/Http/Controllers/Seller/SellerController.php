a7559dc7fbc98de112738efcfbb85739dc574555
KevinMarete <kevomarete@gmail.com>
2020-06-16 16:36:49 +0300

    feat: removed deal views for sellers

--- app/Http/Controllers/Seller/SellerController.php
+++ app/Http/Controllers/Seller/SellerController.php
@@ -788,18 +788,15 @@ class SellerController extends MyController
 
   public function displayOffersTableView(Request $request)
   {
-    $resources = ['offers', 'productdeals'];
+    $resource = 'offers';
     $token = session()->get('token');
     $role_id = session()->get('organization.organization_type.role_id');
     $organization_id = session()->get('organization_id');
 
-    $view_data = [];
-    foreach ($resources as $resource) {
-      $view_data[$resource] = [
-        'table_headers' => $this->getResourceKeys($resource),
-        'table_data' => $this->getResourceData($token, 'organization/' . $organization_id . '/' . $resource)
-      ];
-    }
+    $view_data = [
+      'table_headers' => $this->getResourceKeys($resource),
+      'table_data' => $this->getResourceData($token, 'organization/' . $organization_id . '/' . $resource)
+    ];
 
     $data = [
       'page_title' => 'Offers',
@@ -862,172 +859,6 @@ class SellerController extends MyController
     return view('template.main', $data);
   }
 
-  public function displayNewDealView(Request $request)
-  {
-    $token = session()->get('token');
-    $role_id = session()->get('organization.organization_type.role_id');
-    $organization_id = session()->get('organization_id');
-    $view_data = [
-      'offers' => $this->getResourceData($token, 'organization/' . $organization_id . '/activeoffers'),
-      'productnows' => $this->getResourceData($token, 'organization/' . $organization_id . '/productnows')
-    ];
-    $data = [
-      'page_title' => 'offers',
-      'content_view' => View::make('seller.deals.new', $view_data),
-      'menus' => $this->getRoleMenus($token, $role_id),
-    ];
-
-    return view('template.main', $data);
-  }
-
-  public function saveDeals(Request $request)
-  {
-    $token = session()->get('token');
-    $post_data = $request->all();
-    $organization_id = session()->get('organization_id');
-    $user_id = session()->get('id');
-    $errors = 0;
-    $cost_per_product = env('DEAL_COST');
-
-    //Get organization payment_type
-    $source_url = 'organization/' . $organization_id . '/payment-type';
-    $source_response = $this->manageResourceData($token, 'GET', $source_url, []);
-
-    //Redirect if no payment-type configured
-    if (empty($source_response)) {
-      $flash_msg = '<div class="alert alert-danger alert-dismissible fade show" role="alert">
-                                    <strong>Error!</strong> Please configure payment-type under account tab!
-                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>';
-      $request->session()->flash('bgs_msg', $flash_msg);
-      return redirect('/offers');
-    }
-
-    //Make payment
-    $payment_data = [
-      'method' => $source_response['payment_type']['name'],
-      'amount' => ($cost_per_product * sizeof($post_data['product_now_id'])),
-      'source' => $source_response['payment_type']['details'],
-      'destination' => [
-        'paybill_number' => env('PAYBILL_NUMBER'),
-        'account_number' => env('ACCOUNT_NUMBER')
-      ]
-    ];
-    $payment_response = $this->process_payment($token, $organization_id, $user_id, $payment_data);
-    $payment_id = $payment_response['id'];
-
-    foreach ($post_data['product_now_id'] as $key => $product_now_id) {
-      //Build request object
-      $request_data = [
-        'minimum_order_quantity' => $post_data['minimum_order_quantity'][$key],
-        'offer_id' => $post_data['offer_id'][$key],
-        'product_now_id' => $product_now_id
-      ];
-
-      //Send request data to Api
-      $response = $this->client->post("productdeal", [
-        'headers' => [
-          'Authorization' => 'Bearer ' . session()->get('token')
-        ],
-        'json' => $request_data
-      ]);
-
-      $response = json_decode($response->getBody(), true);
-
-      //Check success
-      if (isset($response['error'])) {
-        $errors += 1;
-      } else {
-        $product_now_id = $response['id'];
-        //Send request data to Api for payment
-        $response = $this->client->post("paymentdeal", [
-          'headers' => [
-            'Authorization' => 'Bearer ' . session()->get('token')
-          ],
-          'json' => ['payment_id' => $payment_id, 'product_deal_id' => $product_now_id]
-        ]);
-      }
-    }
-
-    if ($errors > 0) {
-      $flash_msg = '<div class="alert alert-danger alert-dismissible fade show" role="alert">
-                                <strong>Error!</strong> ' . $errors . ' transactions were not added successfully
-                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>';
-    } else {
-      $flash_msg = '<div class="alert alert-success alert-dismissible fade show" role="alert">
-                            <strong>Success!</strong> Your deal item(s) were added successfully
-                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>';
-    }
-
-    $request->session()->flash('bgs_msg', $flash_msg);
-
-    return redirect('/offers');
-  }
-
-  public function manageDeals(Request $request)
-  {
-    $resource_name = 'productdeals';
-    $singular_resource_name = Str::singular($resource_name);
-    $token = session()->get('token');
-    $role_id = session()->get('organization.organization_type.role_id');
-    $organization_id = session()->get('organization_id');
-    $view_data = [
-      'productnows' => $this->getResourceData($token, 'organization/' . $organization_id . '/productnows'),
-      'offers' => $this->getResourceData($token, 'organization/' . $organization_id . '/offers')
-    ];
-    $view_data['manage_label'] = 'new';
-
-    if ($request->action) {
-      if ($request->action == 'edit') {
-        $view_data['manage_label'] = 'update';
-        $view_data['edit'] = $this->getResourceData($token, $singular_resource_name . '/' . $request->id);
-      } else {
-        if ($request->action == 'new') {
-          $response = $this->manageResourceData($token, 'POST', $singular_resource_name, $request->except('_token'));
-        } else if ($request->action == 'update') {
-          $response = $this->manageResourceData($token, 'PUT', $singular_resource_name . '/' . $request->id, $request->except('_token'));
-        } else if ($request->action == 'delete') {
-          $response = $this->manageResourceData($token, 'DELETE', $singular_resource_name . '/' . $request->id, $request->except('_token'));
-        }
-
-        //Handle response
-        if (isset($response['error'])) {
-          $flash_msg = '<div class="alert alert-danger alert-dismissible fade show" role="alert">
-                                    <strong>Error!</strong> ' . $response["error"] . '
-                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>';
-        } else {
-          $flash_msg = '<div class="alert alert-success alert-dismissible fade show" role="alert">
-                                    <strong>Success!</strong> Deal was managed successfully
-                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>';
-        }
-        $request->session()->flash('bgs_msg', $flash_msg);
-        return redirect('/offers');
-      }
-    }
-
-    $data = [
-      'page_title' => 'Offers',
-      'menus' => $this->getRoleMenus($token, $role_id),
-      'content_view' => View::make('seller.deals.edit', $view_data)
-    ];
-
-    return view('template.main', $data);
-  }
-
   public function getResourceData($token = null, $resource = null)
   {
     $resource_data = [];
@@ -1053,7 +884,6 @@ class SellerController extends MyController
         'stockbalances' => ['brand_name', 'molecular_name', 'pack_size', 'balance'],
         'productnows' => ['id', 'brand_name', 'molecular_name', 'pack_size', 'published'],
         'promotions' => ['id', 'status', 'display_date', 'product'],
-        'productdeals' => ['id', 'brand_name', 'molecular_name', 'min_quantity', 'unit_price', 'discount', 'max_amount']
       ];
       $header_data = $headers[$resource];
     }

71fe89d8b9c7454472b35012b6e99c3cc7f3cba2
KevinMarete <kevomarete@gmail.com>
2020-06-23 00:00:08 +0300

    feat: updated default route for buyer to marketplace

--- resources/views/buyer/marketplace.blade.php
+++ resources/views/buyer/marketplace.blade.php
@@ -1,43 +1,237 @@
 <div class="sb-page-header pb-10 sb-page-header-dark">
 </div>
-<div class="container-fluid mt-n10" style="height: 100vh;">
+<div class="container-fluid mt-n10">
   @if (Session::has('bgs_msg'))
   {!! session('bgs_msg') !!}
   @endif
-  <div class="row mt-2" style="height: 40%">
-    <div class="col-9">
-      <div class="col-12" style="height:100%;border: 1px solid;">
-        Promotion-Slider
-      </div>
-    </div>
-    <div class="col-3">
-      <div class="col-12" style="height:50%;border: 1px solid;">
-        Promotion-Static-1
-      </div>
-      <div class="col-12" style="height:50%;border: 1px solid;">
-        Promotion-Static-2
-      </div>
-    </div>
-  </div>
-  <div class="row mt-2" style="height: 30%">
-    <div class="col-3">
-      <div class="col-12" style="height:50%;border: 1px solid;">
-        Promotion-Static-3
-      </div>
-      <div class="col-12" style="height:50%;border: 1px solid;">
-        Promotion-Static-4
-      </div>
-    </div>
-    <div class="col-9">
-      <div class="col-12" style="height:100%;border: 1px solid;">
-        Top Selling Products
-      </div>
-    </div>
-  </div>
-  <div class="row mt-2" style="height: 30%">
+  <div class="row mt-2">
     <div class="col-12">
-      <div class="col-12" style="height:100%;border: 1px solid;">
-        Offers of the Day
+      <div class="card mb-4">
+        <div class="card-header p-2">
+          <h6 class="mb-0">PROMOTIONS</h6>
+        </div>
+        <div class="card-body">
+          <div class="row">
+            <div class="col-lg-3 col-md-3 col-12 p-2">
+              <div class="row p-2">
+                <div class="col-6 d-flex justify-content-center align-self-center">
+                  <img src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1571750967/Ecommerce/ef192a21ec96.jpg" alt="camera" class="img-fluid">
+                </div>
+                <div class="col-6">
+                  <h6><small>Asus Laptop with 500GB HDD 8GB RAM</small></h6>
+                  <p class="text-primary mb-1">KES 1,200</p>
+                  <a href="#" class="btn btn-sm btn-warning">Shop Now</a>
+                </div>
+              </div>
+              <div class="row p-2">
+                <div class="col-6 d-flex justify-content-center align-self-center">
+                  <img src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1571750967/Ecommerce/ef192a21ec96.jpg" alt="camera" class="img-fluid">
+                </div>
+                <div class="col-6">
+                  <h6><small>Asus Laptop with 500GB HDD 8GB RAM</small></h6>
+                  <p class="text-primary mb-1">KES 1,200</p>
+                  <a href="#" class="btn btn-sm btn-warning">Shop Now</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-12 p-2">
+              <div class="row">
+                <div class="col-12">
+                  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                      <div class="carousel-item active">
+                        <img class="d-block w-100 rounded mx-auto" src="assets/img/promotions/kemsa-slider-4j47ey.jpeg" alt="First slide" height="280">
+                        <div class="carousel-caption d-none d-md-block">
+                          <h5>First Image</h5>
+                          <p>First Paragraph</p>
+                        </div>
+                      </div>
+                      <div class="carousel-item">
+                        <img class="d-block w-100 rounded mx-auto" src="assets/img/promotions/kemsa-slider-4j47ey.jpeg" alt="Second slide" height="280">
+                        <div class="carousel-caption d-none d-md-block">
+                          <h5>Second Image</h5>
+                          <p>Second Paragraph</p>
+                        </div>
+                      </div>
+                      <div class="carousel-item">
+                        <img class="d-block w-100 rounded mx-auto" src="assets/img/promotions/kemsa-slider-4j47ey.jpeg" alt="Third slide" height="280">
+                        <div class="carousel-caption d-none d-md-block">
+                          <h5>Third Image</h5>
+                          <p>Third Paragraph</p>
+                        </div>
+                      </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Next</span>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-12 p-2">
+              <div class="row p-2">
+                <div class="col-6 d-flex justify-content-center align-self-center">
+                  <img src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1571750967/Ecommerce/ef192a21ec96.jpg" alt="camera" class="img-fluid">
+                </div>
+                <div class="col-6">
+                  <h6><small>Asus Laptop with 500GB HDD 8GB RAM</small></h6>
+                  <p class="text-primary mb-1">KES 1,200</p>
+                  <a href="#" class="btn btn-sm btn-warning">Shop Now</a>
+                </div>
+              </div>
+              <div class="row p-2">
+                <div class="col-6 d-flex justify-content-center align-self-center">
+                  <img src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1571750967/Ecommerce/ef192a21ec96.jpg" alt="camera" class="img-fluid">
+                </div>
+                <div class="col-6">
+                  <h6><small>Asus Laptop with 500GB HDD 8GB RAM</small></h6>
+                  <p class="text-primary mb-1">KES 1,200</p>
+                  <a href="#" class="btn btn-sm btn-warning">Shop Now</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row mt-2">
+    <div class="col-12">
+      <div class="card mb-4">
+        <div class="card-header p-2">
+          <h6 class="mb-0">OFFERS OF THE DAY</h6>
+          <a href="/offers-day" class="ml-auto p-2">View All Offers</a>
+        </div>
+        <div class="card-body">
+          <div class="row">
+            <div class="col-lg-2 col-md-2 col-sm-4 col-6 p-2">
+              <div class="product tumbnail thumbnail-3"><a href="#"><img src="http://static.livedemo00.template-help.com/wt_58434/images/shop-1.jpg" alt="" height="80"></a>
+                <div class="caption">
+                  <h6><a href="#">Short Sleeve T-Shirt</a></h6><span class="price">
+                    <del>$24.99</del></span><span class="price sale">$12.49</span>
+                </div>
+                <a href="#" class="btn btn-sm btn-warning">Shop Now</a>
+              </div>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-4 col-6 p-2">
+              <div class="product tumbnail thumbnail-3"><a href="#"><img src="http://static.livedemo00.template-help.com/wt_58434/images/shop-1.jpg" alt="" height="80"></a>
+                <div class="caption">
+                  <h6><a href="#">Short Sleeve T-Shirt</a></h6><span class="price">
+                    <del>$24.99</del></span><span class="price sale">$12.49</span>
+                </div>
+                <a href="#" class="btn btn-sm btn-warning">Shop Now</a>
+              </div>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-4 col-6 p-2">
+              <div class="product tumbnail thumbnail-3"><a href="#"><img src="http://static.livedemo00.template-help.com/wt_58434/images/shop-1.jpg" alt="" height="80"></a>
+                <div class="caption">
+                  <h6><a href="#">Short Sleeve T-Shirt</a></h6><span class="price">
+                    <del>$24.99</del></span><span class="price sale">$12.49</span>
+                </div>
+                <a href="#" class="btn btn-sm btn-warning">Shop Now</a>
+              </div>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-4 col-6 p-2">
+              <div class="product tumbnail thumbnail-3"><a href="#"><img src="http://static.livedemo00.template-help.com/wt_58434/images/shop-2.jpg" alt="" height="80"></a>
+                <div class="caption">
+                  <h6><a href="#">Short Sleeve T-Shirt</a></h6><span class="price">
+                    <del>$24.99</del></span><span class="price sale">$12.49</span>
+                </div>
+                <a href="#" class="btn btn-sm btn-warning">Shop Now</a>
+              </div>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-4 col-6 p-2">
+              <div class="product tumbnail thumbnail-3"><a href="#"><img src="http://static.livedemo00.template-help.com/wt_58434/images/shop-3.jpg" alt="" height="80"></a>
+                <div class="caption">
+                  <h6><a href="#">Short Sleeve T-Shirt</a></h6><span class="price">
+                    <del>$12.49</del></span><span class="price sale">$12.49</span>
+                </div>
+                <a href="#" class="btn btn-sm btn-warning">Shop Now</a>
+              </div>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-4 col-6 p-2">
+              <div class="product tumbnail thumbnail-3"><a href="#"><img src="http://static.livedemo00.template-help.com/wt_58434/images/shop-4.jpg" alt="" height="80"></a>
+                <div class="caption">
+                  <h6><a href="#">Short Sleeve T-Shirt</a></h6><span class="price">
+                    <del>$24.99</del></span><span class="price sale">$12.49</span>
+                </div>
+                <a href="#" class="btn btn-sm btn-warning">Shop Now</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row mt-2">
+    <div class="col-12">
+      <div class="card mb-4">
+        <div class="card-header p-2">
+          <h6 class="mb-0">TOP SELLING PRODUCTS</h6>
+          <a href="/ordernow" class="ml-auto p-2">View All Products</a>
+        </div>
+        <div class="card-body">
+          <div class="row">
+            <div class="col-lg-2 col-md-2 col-sm-4 col-6 p-2">
+              <div class="product tumbnail thumbnail-3"><a href="#"><img src="http://static.livedemo00.template-help.com/wt_58434/images/shop-1.jpg" alt="" height="80"></a>
+                <div class="caption">
+                  <h6><a href="#">Short Sleeve T-Shirt</a></h6><span class="price">$24.99</span>
+                </div>
+                <a href="#" class="btn btn-sm btn-warning">Shop Now</a>
+              </div>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-4 col-6 p-2">
+              <div class="product tumbnail thumbnail-3"><a href="#"><img src="http://static.livedemo00.template-help.com/wt_58434/images/shop-2.jpg" alt="" height="80"></a>
+                <div class="caption">
+                  <h6><a href="#">Short Sleeve T-Shirt</a></h6><span class="price">$24.99</span>
+                </div>
+                <a href="#" class="btn btn-sm btn-warning">Shop Now</a>
+              </div>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-4 col-6 p-2">
+              <div class="product tumbnail thumbnail-3"><a href="#"><img src="http://static.livedemo00.template-help.com/wt_58434/images/shop-3.jpg" alt="" height="80"></a>
+                <div class="caption">
+                  <h6><a href="#">Short Sleeve T-Shirt</a></h6><span class="price">$12.49</span>
+                </div>
+                <a href="#" class="btn btn-sm btn-warning">Shop Now</a>
+              </div>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-4 col-6 p-2">
+              <div class="product tumbnail thumbnail-3"><a href="#"><img src="http://static.livedemo00.template-help.com/wt_58434/images/shop-4.jpg" alt="" height="80"></a>
+                <div class="caption">
+                  <h6><a href="#">Short Sleeve T-Shirt</a></h6><span class="price">$24.99</span>
+                </div>
+                <a href="#" class="btn btn-sm btn-warning">Shop Now</a>
+              </div>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-4 col-6 p-2">
+              <div class="product tumbnail thumbnail-3"><a href="#"><img src="http://static.livedemo00.template-help.com/wt_58434/images/shop-3.jpg" alt="" height="80"></a>
+                <div class="caption">
+                  <h6><a href="#">Short Sleeve T-Shirt</a></h6><span class="price">$12.49</span>
+                </div>
+                <a href="#" class="btn btn-sm btn-warning">Shop Now</a>
+              </div>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-4 col-6 p-2">
+              <div class="product tumbnail thumbnail-3"><a href="#"><img src="http://static.livedemo00.template-help.com/wt_58434/images/shop-4.jpg" alt="" height="80"></a>
+                <div class="caption">
+                  <h6><a href="#">Short Sleeve T-Shirt</a></h6><span class="price">$24.99</span>
+                </div>
+                <a href="#" class="btn btn-sm btn-warning">Shop Now</a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>

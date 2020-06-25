16d9f93017514709fe5cfb2955a6d3049cee0664
KevinMarete <kevomarete@gmail.com>
2020-06-26 00:51:54 +0300

    feat: added middleware to restrict access for private routes without sessions

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,6 +63,7 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'json.response' => \App\Http\Middleware\ForceJsonResponse::class,
         'cors' => \App\Http\Middleware\Cors::class,
+        'usersession' => \App\Http\Middleware\CheckUserSession::class,
     ];
 
     /**

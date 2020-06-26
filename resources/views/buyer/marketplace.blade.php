100980e392ea0b450fa97fa9ede0ecebff09b059
KevinMarete <kevomarete@gmail.com>
2020-06-27 00:38:59 +0300

    feat: adjusted static promotion placement

--- resources/views/buyer/marketplace.blade.php
+++ resources/views/buyer/marketplace.blade.php
@@ -12,21 +12,32 @@
         </div>
         <div class="card-body">
           <div class="row">
-            <div class="col-lg-3 col-md-3 col-12 p-2">
+            <div class="col-lg-2 col-md-2 col-12 p-0">
               @foreach ($promotions['static-left'] as $promotion)
-              <div class="row p-2">
-                <div class="col-6 d-flex justify-content-center align-self-center">
-                  <img src="{{ $promotion['display_url'] }}" alt="camera" class="img-fluid">
-                </div>
-                <div class="col-6">
-                  <h6><small>{{ $promotion['product_now']['product']['brand_name'] }}</small></h6>
-                  <p class="text-primary mb-1">KES {{ number_format($promotion['product_now']['unit_price'], 2) }}</p>
+              <div class="text-center pb-2">
+                <div class="product tumbnail thumbnail-3">
+                  @if(!$promotion['is_promotion'])
+                  <a href="#"><img src="{{ $promotion['display_url'] }}" alt="" height="80"></a>
+                  <div class="caption">
+                    <h6 class="text-truncate" data-toggle="tooltip" data-placement="top" title="{{ $promotion['product_now']['product']['brand_name'] }}">
+                      <small>{{ $promotion['product_now']['product']['brand_name'] }}</small>
+                    </h6>
+                    <span class="price">KES {{ number_format($promotion['product_now']['unit_price'], 2) }}</span>
+                  </div>
                   <a href="/ordernow/{{ $promotion['product_now']['product_id'].'/'.$promotion['product_now']['organization_id'] }}" class="btn btn-sm btn-warning">Shop Now</a>
+                  @else
+                  <div class="img-wrapper">
+                    <img class="img-responsive mb-1 w-100 rounded" height="190" src="{{ $promotion['display_url'] }}">
+                    <div class="img-overlay text-right">
+                      <a href="/ordernow/{{ $promotion['product_now']['product_id'].'/'.$promotion['product_now']['organization_id'] }}" class="btn btn-sm btn-warning">Shop Now</a>
+                    </div>
+                  </div>
+                  @endif
                 </div>
               </div>
               @endforeach
             </div>
-            <div class="col-lg-6 col-md-6 col-12 p-2">
+            <div class="col-lg-8 col-md-8 col-12">
               <div class="row">
                 <div class="col-12">
                   <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
@@ -38,11 +49,11 @@
                     </ol>
                     <div class="carousel-inner">
                       <div class="carousel-item active">
-                        <img class="d-block w-100 rounded mx-auto" src="{{ env('PROMOTIONS_DEFAULT_IMAGE') }}" alt="Default Promotion" height="280">
+                        <img class="d-block w-100 rounded mx-auto" src="{{ env('PROMOTIONS_DEFAULT_IMAGE') }}" alt="Default Promotion" height="395">
                       </div>
                       @foreach ($promotions['slider'] as $promotion)
                       <div class="carousel-item">
-                        <img class="d-block w-100 rounded mx-auto" src="{{ $promotion['display_url'] }}" alt="Promotion Image" height="280">
+                        <img class="d-block w-100 rounded mx-auto" src="{{ $promotion['display_url'] }}" alt="Promotion Image" height="395">
                         <div class="carousel-caption d-none d-md-block text-right">
                           <a href="/ordernow/{{ $promotion['product_now']['product_id'].'/'.$promotion['product_now']['organization_id'] }}" class="btn btn-sm btn-warning">Shop Now</a>
                         </div>
@@ -61,16 +72,27 @@
                 </div>
               </div>
             </div>
-            <div class="col-lg-3 col-md-3 col-12 p-2">
+            <div class="col-lg-2 col-md-2 col-12">
               @foreach ($promotions['static-right'] as $promotion)
-              <div class="row p-2">
-                <div class="col-6 d-flex justify-content-center align-self-center">
-                  <img src="{{ $promotion['display_url'] }}" alt="camera" class="img-fluid">
-                </div>
-                <div class="col-6">
-                  <h6><small>{{ $promotion['product_now']['product']['brand_name'] }}</small></h6>
-                  <p class="text-primary mb-1">KES {{ number_format($promotion['product_now']['unit_price'], 2) }}</p>
+              <div class="text-center p-2">
+                <div class="product tumbnail thumbnail-3">
+                  @if(!$promotion['is_promotion'])
+                  <a href="#"><img src="{{ $promotion['display_url'] }}" alt="" height="80"></a>
+                  <div class="caption">
+                    <h6 class="text-truncate" data-toggle="tooltip" data-placement="top" title="{{ $promotion['product_now']['product']['brand_name'] }}">
+                      <small>{{ $promotion['product_now']['product']['brand_name'] }}</small>
+                    </h6>
+                    <span class="price">KES {{ number_format($promotion['product_now']['unit_price'], 2) }}</span>
+                  </div>
                   <a href="/ordernow/{{ $promotion['product_now']['product_id'].'/'.$promotion['product_now']['organization_id'] }}" class="btn btn-sm btn-warning">Shop Now</a>
+                  @else
+                  <div class="img-wrapper">
+                    <img class="img-responsive mb-1 w-100 rounded" height="190" src="{{ $promotion['display_url'] }}">
+                    <div class="img-overlay text-right">
+                      <a href="/ordernow/{{ $promotion['product_now']['product_id'].'/'.$promotion['product_now']['organization_id'] }}" class="btn btn-sm btn-warning">Shop Now</a>
+                    </div>
+                  </div>
+                  @endif
                 </div>
               </div>
               @endforeach

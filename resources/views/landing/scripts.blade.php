9f6fc88961a34b9a98fdf36ee6414be99db4a149
KevinMarete <kevomarete@gmail.com>
2020-07-17 22:03:21 +0300

    feat: added local aos css and js references

--- resources/views/landing/scripts.blade.php
+++ resources/views/landing/scripts.blade.php
@@ -1,7 +1,7 @@
-<script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+<script src="libs/jquery/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
+<script src="libs/bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 <script src="js/landing.js"></script>
-<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+<script src="libs/aos/js/aos.js"></script>
 <script>
   AOS.init({
     disable: 'mobile',
@@ -10,5 +10,7 @@
   });
 </script>
 
+<!--
 <script src="js/sb-customizer.js"></script>
 <sb-customizer project="sb-ui-kit-pro"></sb-customizer>
+-->

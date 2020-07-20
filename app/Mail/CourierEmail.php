6f39ce87cbf4b3088321ac75f0ebb7421a07bbbc
KevinMarete <kevomarete@gmail.com>
2020-07-20 14:04:37 +0300

    feat: updated rfq reference to pharmahub

--- app/Mail/CourierEmail.php
+++ app/Mail/CourierEmail.php
@@ -30,6 +30,6 @@ class CourierEmail extends Mailable
     {
         $this->view('emails/courier_notification');
         return $this->to($this->order->courier->email)
-                    ->subject('BGS Order #'.$this->order->id.' Pickup and Delivery');
+                    ->subject('Pharmahub Order #'.$this->order->id.' Pickup and Delivery');
     }
 }

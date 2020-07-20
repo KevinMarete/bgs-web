156fbaad8d744b4a1fe03a517451bb6daa8fa4ee
KevinMarete <kevomarete@gmail.com>
2020-07-20 14:01:55 +0300

    feat: added logo image to header

--- resources/views/landing/header.blade.php
+++ resources/views/landing/header.blade.php
@@ -1,6 +1,10 @@
 <nav class="navbar navbar-marketing navbar-expand-lg bg-transparent navbar-dark fixed-top">
   <div class="container">
-    <a class="navbar-brand text-warning" href="/">Pharmahub</a><button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><i data-feather="menu"></i></button>
+    <a class="navbar-brand text-warning d-none d-sm-block text-center" href="/">
+      <img src="assets/img/logo.jpg" alt="logo" class="rounded d-inline-block align-middle">
+      Pharmahub
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><i data-feather="menu"></i></button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ml-auto mr-lg-5">
         <li class="nav-item"><a class="nav-link text-warning" href="/home">Home </a></li>

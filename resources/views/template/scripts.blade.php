8af4c4bdface2b19bee43ba2c91846e336f9541c
KevinMarete <kevomarete@gmail.com>
2020-07-29 20:54:06 +0300

    feat: added tawk support

--- resources/views/template/scripts.blade.php
+++ resources/views/template/scripts.blade.php
@@ -11,6 +11,21 @@
 <script src="/libs/datatables/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
 <script src="/libs/datatables/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
 <script src="/assets/demo/datatables-demo.js"></script>
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+  var Tawk_API = Tawk_API || {},
+    Tawk_LoadStart = new Date();
+  (function() {
+    var s1 = document.createElement("script"),
+      s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = 'https://embed.tawk.to/5f2028385e51983a11f5f0c0/default';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
+  })();
+</script>
 <!--
 <script src="/js/sb-customizer.js"></script>
 <sb-customizer project="sb-admin-pro"></sb-customizer>

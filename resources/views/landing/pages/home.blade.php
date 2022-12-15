5e559cc9455e70e77c888bf3f2b7a808fefa9981
Kevin Marete <kevinmmarete@gmail.com>
2022-12-15 18:13:12 -0500

    Display organization type totals on home landing page

--- resources/views/landing/pages/home.blade.php
+++ resources/views/landing/pages/home.blade.php
@@ -1,133 +1,203 @@
 <header class="page-header page-header-dark bg-gradient-primary-to-secondary bg-landing-img">
-  <div class="page-header-content pt-10">
-    <div class="container">
-      <div class="row align-items-center">
-        <div class="col-lg-6" data-aos="fade-up">
-          <h1 class="page-header-title text-dark">Welcome to {{ config('app.label') }}</h1>
-          <p class="page-header-text mb-5">
-            <p class="text-dark">A marketplace for buyers (pharmacies, hospitals etc..) and sellers (suppliers, manufacturers etc..) to provide quality healthcare products at the lowest possible prices.</p>
-          </p>
-          <a class="btn btn-warning btn-marketing rounded-pill lift lift-sm" href="#buyers">
-            Buyers <i class="fas fa-clinic-medical ml-1"></i></a>
-          <a class="btn btn-warning btn-marketing rounded-pill lift lift-sm" href="#sellers">
-            Sellers <i class="fas fa-medkit ml-1"></i></a>
+    <div class="page-header-content pt-10">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6" data-aos="fade-up">
+                    <h1 class="page-header-title text-dark">Welcome to {{ config('app.label') }}</h1>
+                    <p class="page-header-text mb-5">
+                    <p class="text-dark">A marketplace for buyers (pharmacies, hospitals etc..) and sellers (suppliers,
+                        manufacturers etc..) to provide quality healthcare products at the lowest possible prices.</p>
+                    </p>
+                    <a class="btn btn-warning btn-marketing rounded-pill lift lift-sm" href="#buyers">
+                        Buyers <i class="fas fa-clinic-medical ml-1"></i></a>
+                    <a class="btn btn-warning btn-marketing rounded-pill lift lift-sm" href="#sellers">
+                        Sellers <i class="fas fa-medkit ml-1"></i></a>
+                </div>
+                <div class="col-lg-6 d-none d-lg-block" data-aos="fade-up" data-aos-delay="50"><img
+                        class="img-fluid rounded" src="assets/img/landing/home.gif"/></div>
+            </div>
         </div>
-        <div class="col-lg-6 d-none d-lg-block" data-aos="fade-up" data-aos-delay="50"><img class="img-fluid rounded" src="assets/img/landing/home.gif" /></div>
-      </div>
     </div>
-  </div>
-  <div class="svg-border-rounded text-white">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 144.54 17.34" preserveAspectRatio="none" fill="currentColor">
-      <path d="M144.54,17.34H0V0H144.54ZM0,0S32.36,17.34,72.27,17.34,144.54,0,144.54,0" /></svg>
-  </div>
+    <div class="svg-border-rounded text-white">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 144.54 17.34" preserveAspectRatio="none"
+             fill="currentColor">
+            <path d="M144.54,17.34H0V0H144.54ZM0,0S32.36,17.34,72.27,17.34,144.54,0,144.54,0"/>
+        </svg>
+    </div>
 </header>
 <section class="bg-white py-10" id="buyers">
-  <div class="container">
-    <div class="row align-items-center justify-content-center">
-      <div class="col-md-9 col-lg-4 order-1 order-lg-0" data-aos="fade-right">
-        <div class="content-skewed content-skewed-right">
-          <img class="content-skewed-item img-fluid shadow-lg rounded-lg" src="assets/img/landing/buyers.png" />
+    <div class="container">
+        <div class="row align-items-center justify-content-center">
+            <div class="col-md-9 col-lg-4 order-1 order-lg-0" data-aos="fade-right">
+                <div class="content-skewed content-skewed-right">
+                    <img class="content-skewed-item img-fluid shadow-lg rounded-lg"
+                         src="assets/img/landing/buyers.png"/>
+                </div>
+            </div>
+            <div class="col-lg-8 order-0 order-lg-1 mb-5 mb-lg-0" data-aos="fade-left">
+                <div class="mt-2 mb-3">
+                    <h2>Buyers Package</h2>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 mb-4">
+                        <h6>Our Solution</h6>
+                        <p class="mb-2 small mb-0"><span class="text-warning">eACCESS&trade;</span> - A free electronic
+                            ordering system for all your needs from your supplier</p>
+                        <ul>
+                            <li class="small">Any time any where orders are delivered electronically on a real time to
+                                your supplier
+                            </li>
+                            <li class="small">Place your orders at your convenient time</li>
+                            <li class="small">Timely delivery</li>
+                        </ul>
+                        <p class="mb-2 small mb-0"><span class="text-success">eRFQ&trade;</span> - Get quotations from
+                            two or more suppliers</p>
+                    </div>
+                    <div class="col-md-6 mb-4">
+                        <h6>Our Promise</h6>
+                        <ul>
+                            <li class="mb-2 small">Lowest Prices</li>
+                            <li class="mb-2 small">Quality Products</li>
+                            <li class="mb-2 small">+10k Products to Choose</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 mb-4">
+                        <h6>What we Offer</h6>
+                        <ul>
+                            <li class="mb-2 small mb-0">Access to negotiated lowest prices</li>
+                            <li class="mb-2 small mb-0">Price comparison from other suppliers</li>
+                            <li class="mb-2 small mb-0">Access to over 10,000 pharmaceutical products in one platform
+                            </li>
+                            <li class="mb-2 small mb-0">Easy automated online ordering process-any time, anywhere</li>
+                            <li class="mb-2 small mb-0">Access to daily offers and deals from suppliers</li>
+                            <li class="mb-2 small mb-0">Access to credit facilities from partner banks through the
+                                security of order transactions in Pharma Hub
+                            </li>
+                            <li class="mb-2 small mb-0">Access to quality products-PPB registered</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-      <div class="col-lg-8 order-0 order-lg-1 mb-5 mb-lg-0" data-aos="fade-left">
-        <div class="mt-2 mb-3">
-          <h2>Buyers Package</h2>
-        </div>
-        <div class="row">
-          <div class="col-md-6 mb-4">
-            <h6>Our Solution</h6>
-            <p class="mb-2 small mb-0"><span class="text-warning">eACCESS&trade;</span> - A free electronic ordering system for all your needs from your supplier</p>
-            <ul>
-              <li class="small">Any time any where orders are delivered electronically on a real time to your supplier</li>
-              <li class="small">Place your orders at your convenient time</li>
-              <li class="small">Timely delivery</li>
-            </ul>
-            <p class="mb-2 small mb-0"><span class="text-success">eRFQ&trade;</span> - Get quotations from two or more suppliers</p>
-          </div>
-          <div class="col-md-6 mb-4">
-            <h6>Our Promise</h6>
-            <ul>
-              <li class="mb-2 small">Lowest Prices</li>
-              <li class="mb-2 small">Quality Products</li>
-              <li class="mb-2 small">+10k Products to Choose</li>
-            </ul>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-12 mb-4">
-            <h6>What we Offer</h6>
-            <ul>
-              <li class="mb-2 small mb-0">Access to negotiated lowest prices</li>
-              <li class="mb-2 small mb-0">Price comparison from other suppliers</li>
-              <li class="mb-2 small mb-0">Access to over 10,000 pharmaceutical products in one platform</li>
-              <li class="mb-2 small mb-0">Easy automated online ordering process-any time, anywhere</li>
-              <li class="mb-2 small mb-0">Access to daily offers and deals from suppliers</li>
-              <li class="mb-2 small mb-0">Access to credit facilities from partner banks through the security of order transactions in Pharma Hub</li>
-              <li class="mb-2 small mb-0">Access to quality products-PPB registered</li>
-            </ul>
-          </div>
-        </div>
-      </div>
     </div>
-  </div>
-  <div class="svg-border-rounded text-light">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 144.54 17.34" preserveAspectRatio="none" fill="currentColor">
-      <path d="M144.54,17.34H0V0H144.54ZM0,0S32.36,17.34,72.27,17.34,144.54,0,144.54,0" /></svg>
-  </div>
+    <div class="svg-border-rounded text-light">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 144.54 17.34" preserveAspectRatio="none"
+             fill="currentColor">
+            <path d="M144.54,17.34H0V0H144.54ZM0,0S32.36,17.34,72.27,17.34,144.54,0,144.54,0"/>
+        </svg>
+    </div>
 </section>
 <section class="bg-light py-10" id="sellers">
-  <div class="container">
-    <div class="row align-items-center justify-content-center">
-      <div class="col-md-9 col-lg-4 order-1 order-lg-0" data-aos="fade-right">
-        <div class="content-skewed content-skewed-right">
-          <img class="content-skewed-item img-fluid shadow-lg rounded-lg" src="assets/img/landing/sellers.png" />
+    <div class="container">
+        <div class="row align-items-center justify-content-center">
+            <div class="col-md-9 col-lg-4 order-1 order-lg-0" data-aos="fade-right">
+                <div class="content-skewed content-skewed-right">
+                    <img class="content-skewed-item img-fluid shadow-lg rounded-lg"
+                         src="assets/img/landing/sellers.png"/>
+                </div>
+            </div>
+            <div class="col-lg-8 order-0 order-lg-1 mb-5 mb-lg-0" data-aos="fade-left">
+                <div class="mt-2 mb-3">
+                    <h2>Sellers Package</h2>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 mb-4">
+                        <h6>Our Solutions</h6>
+                        <p class="mb-2 small mb-0"><span class="text-warning">eCAT&trade;</span> - Electronic Pricelist
+                            Visibility to +1000 chemists and Hospital on a daily basis.<br/>
+                            Direct orders from Chemists and Hospitals.
+                        </p>
+                        <p class="mb-2 small mb-0"><span class="text-dark">ePROM&trade;</span> - Electronic Brand
+                            Visibility to +1000 chemists and Hospital on a daily basis. <br/>
+                            Direct orders from Chemists and Hospitals.
+                        </p>
+                        <p class="mb-2 small mb-0"><span class="text-primary">eDEAL&trade;</span> - Electronic Offers
+                            Visibility to +1000 chemists and Hospital on a daily basis. <br/>
+                            Direct orders from Chemists and Hospitals.
+                        </p>
+                        <p class="mb-2 small mb-0"><span class="text-success">eRFQ&trade;</span> - Electronic Quote
+                            Visibility from +1000 chemists and Hospital on a daily basis. <br/>
+                            Direct orders from Chemists and Hospitals.
+                        </p>
+                    </div>
+                    <div class="col-md-6 mb-4">
+                        <h6>Our Promise</h6>
+                        <ul>
+                            <li class="mb-2 small">Increased brand visibility</li>
+                            <li class="mb-2 small">Increase sales</li>
+                            <li class="mb-2 small">Reduced marketing costs</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 mb-4">
+                        <h6>What we Offer</h6>
+                        <ul>
+                            <li class="mb-2 small mb-0"><span class="font-weight-bold text-warning">Widest</span> market
+                                reach for your Brands, +1000 chemists & Hospitals
+                            </li>
+                            <li class="mb-2 small mb-0"><span class="font-weight-bold text-warning">Cheapest</span> way
+                                to introduce new products in the market
+                            </li>
+                            <li class="mb-2 small mb-0"><span class="font-weight-bold text-warning">Fastest</span> way
+                                to increase sales
+                            </li>
+                            <li class="mb-2 small mb-0"><span class="font-weight-bold text-warning">Best</span>
+                                supplement to field sales representatives
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-      <div class="col-lg-8 order-0 order-lg-1 mb-5 mb-lg-0" data-aos="fade-left">
-        <div class="mt-2 mb-3">
-          <h2>Sellers Package</h2>
-        </div>
-        <div class="row">
-          <div class="col-md-6 mb-4">
-            <h6>Our Solutions</h6>
-            <p class="mb-2 small mb-0"><span class="text-warning">eCAT&trade;</span> - Electronic Pricelist Visibility to +1000 chemists and Hospital on a daily basis.<br />
-              Direct orders from Chemists and Hospitals.
-            </p>
-            <p class="mb-2 small mb-0"><span class="text-dark">ePROM&trade;</span> - Electronic Brand Visibility to +1000 chemists and Hospital on a daily basis. <br />
-              Direct orders from Chemists and Hospitals.
-            </p>
-            <p class="mb-2 small mb-0"><span class="text-primary">eDEAL&trade;</span> - Electronic Offers Visibility to +1000 chemists and Hospital on a daily basis. <br />
-              Direct orders from Chemists and Hospitals.
-            </p>
-            <p class="mb-2 small mb-0"><span class="text-success">eRFQ&trade;</span> - Electronic Quote Visibility from +1000 chemists and Hospital on a daily basis. <br />
-              Direct orders from Chemists and Hospitals.
-            </p>
-          </div>
-          <div class="col-md-6 mb-4">
-            <h6>Our Promise</h6>
-            <ul>
-              <li class="mb-2 small">Increased brand visibility</li>
-              <li class="mb-2 small">Increase sales</li>
-              <li class="mb-2 small">Reduced marketing costs</li>
-            </ul>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-12 mb-4">
-            <h6>What we Offer</h6>
-            <ul>
-              <li class="mb-2 small mb-0"><span class="font-weight-bold text-warning">Widest</span> market reach for your Brands, +1000 chemists & Hospitals</li>
-              <li class="mb-2 small mb-0"><span class="font-weight-bold text-warning">Cheapest</span> way to introduce new products in the market</li>
-              <li class="mb-2 small mb-0"><span class="font-weight-bold text-warning">Fastest</span> way to increase sales</li>
-              <li class="mb-2 small mb-0"><span class="font-weight-bold text-warning">Best</span> supplement to field sales representatives</li>
-            </ul>
-          </div>
-        </div>
-      </div>
     </div>
-  </div>
-  <div class="svg-border-rounded text-success">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 144.54 17.34" preserveAspectRatio="none" fill="currentColor">
-      <path d="M144.54,17.34H0V0H144.54ZM0,0S32.36,17.34,72.27,17.34,144.54,0,144.54,0" /></svg>
-  </div>
+    <div class="svg-border-rounded text-light">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 144.54 17.34" preserveAspectRatio="none"
+             fill="currentColor">
+            <path d="M144.54,17.34H0V0H144.54ZM0,0S32.36,17.34,72.27,17.34,144.54,0,144.54,0"/>
+        </svg>
+    </div>
+</section>
+<section class="bg-white py-10" id="totals">
+    <div class="container">
+        <div class="row align-items-center justify-content-center">
+            <div class="col-lg-6 order-1 order-lg-0" data-aos="fade-right">
+                <div class="content-skewed content-skewed-right">
+                    <div class="card-deck">
+                        @foreach($categories as $category)
+                            <div class="card">
+                                <div class="card-header d-flex justify-content-center align-content-center">
+                                    <em class="{{ $category["icon"] }} display-4 text-warning"></em>
+                                </div>
+                                <div class="card-body">
+                                    <div class="card-text d-flex justify-content-between align-content-center">
+                                        <p class="display-3 text-warning">{{ $category["total"] }}</p>
+                                        <p class="display-6 text-secondary">{{ $category["name"] }}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 order-0 order-lg-1 mb-5 mb-lg-0" data-aos="fade-left">
+                <div class="mt-2 mb-3">
+                    <h2>Partner Distribution</h2>
+                </div>
+                <div class="row">
+                    <div class="col-md-12 mb-4">
+                        MAP HERE!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="svg-border-rounded text-success">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 144.54 17.34" preserveAspectRatio="none"
+             fill="currentColor">
+            <path d="M144.54,17.34H0V0H144.54ZM0,0S32.36,17.34,72.27,17.34,144.54,0,144.54,0"/>
+        </svg>
+    </div>
 </section>

f5e3b90a0cd89bd0c6b7757680e78e6cb6c0a211
Kevin Marete <49183352+KevinMarete@users.noreply.github.com>
2022-12-15 18:14:18 -0500

    [BGS] Category Dashboard (#41)
    * Get organization type totals
    
    * Display organization type totals on home landing page

--- app/Http/Controllers/Auth/LandingController.php
+++ app/Http/Controllers/Auth/LandingController.php
@@ -9,40 +9,68 @@ use Illuminate\Support\Facades\View;
 
 class LandingController extends Controller
 {
-  protected $client;
+    protected $client;
 
-  public function __construct()
-  {
-    //Setup Curl client
-    $this->client = new Client([
-      'base_uri' => env('API_URL'),
-      'defaults' => [
-        'exceptions' => false
-      ],
-      'timeout'  => 10.0
-    ]);
-  }
+    public function __construct()
+    {
+        //Setup Curl client
+        $this->client = new Client([
+            'base_uri' => env('API_URL'),
+            'defaults' => [
+                'exceptions' => false
+            ],
+            'timeout' => 10.0
+        ]);
+    }
 
-  public function displayView(Request $request)
-  {
-    $page_name = (!$request->segment(1)) ? 'home' : $request->segment(1);
+    public function displayView(Request $request)
+    {
+        $page_name = (!$request->segment(1)) ? 'home' : $request->segment(1);
+        $organizations = $this->getOrganizations();
+        $organization_type_totals = $this->getOrganizationTypeTotals($organizations);
+        $view_data = [
+            'organizations' => $organizations,
+            'categories' => [
+                [
+                    "name" => "Pharmacies",
+                    "icon" => "fa fa-pills",
+                    "total" => ($organization_type_totals['pharmacy'] ?? 0) + ($organization_type_totals['chemist'] ?? 0)],
+                [
+                    "name" => "Hospitals",
+                    "icon" => "fa fa-hospital",
+                    "total" => ($organization_type_totals['hospital'] ?? 0) + ($organization_type_totals['clinic'] ?? 0)],
+                [
+                    "name" => "Suppliers",
+                    "icon" => "fa fa-truck",
+                    "total" => ($organization_type_totals['supplier'] ?? 0) + ($organization_type_totals['distributor'] ?? 0)
+                ]
+            ]
+        ];
+        $data = [
+            'page_title' => $page_name,
+            'content_view' => View::make('landing.pages.' . $page_name, $view_data)
+        ];
+        return view('landing.main', $data);
+    }
 
-    $view_data = [
-      'organizations' => $this->getOrganizations()
-    ];
+    public function getOrganizations()
+    {
+        $request = $this->client->get('organizations');
+        $response = $request->getBody();
+        $organizations = json_decode($response, true);
+        return $organizations;
+    }
 
-    $data = [
-      'page_title' => $page_name,
-      'content_view' => View::make('landing.pages.' . $page_name, $view_data)
-    ];
-    return view('landing.main', $data);
-  }
-
-  public function getOrganizations()
-  {
-    $request = $this->client->get('organizations');
-    $response = $request->getBody();
-    $organizations = json_decode($response, true);
-    return $organizations;
-  }
+    public function getOrganizationTypeTotals($organizations)
+    {
+        return array_reduce($organizations, function ($carry, $organization) {
+            $role_name = strtolower($organization['organization_type']['name']);
+            if (!isset($carry[$role_name])) {
+                $carry[$role_name] = 1;
+            } else {
+                $carry[$role_name]++;
+            }
+            return $carry;
+        });
+    }
 }

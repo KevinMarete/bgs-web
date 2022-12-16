09ae9387272cf406b00f8e0f4e4ab3b295d8a14e
Kevin Marete <kevinmmarete@gmail.com>
2022-12-15 21:44:37 -0500

    Added leaflet script for adding markers to map

--- resources/views/landing/scripts.blade.php
+++ resources/views/landing/scripts.blade.php
@@ -1,6 +1,6 @@
 <script src="libs/jquery/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
 <script src="libs/bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-<script src="libs/leaflet/1.9.3/js/leaflet.js"></script>
+<script src="libs/leaflet/1.9.3/leaflet.js"></script>
 <script src="js/landing.js"></script>
 <script src="libs/aos/js/aos.js"></script>
 <script>
@@ -10,7 +10,19 @@
     once: true
   });
 </script>
+<script type="text/javascript">
+    //Leaflet map with Kenya Geo Coordinates
+    const map = L.map("distribution_map").setView([-1.286389, 36.817223], 10);
 
+    //Add tile layer
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {maxZoom: 19}).addTo(map);
+
+    //Get markers and add them to map
+    const markers = JSON.parse(document.querySelector("#partner_geo_coordinates").getAttribute("data-coordinates"));
+    for(const marker of markers){
+        L.marker(marker).addTo(map);
+    }
+</script>
 <!--
 <script src="js/sb-customizer.js"></script>
 <sb-customizer project="sb-ui-kit-pro"></sb-customizer>

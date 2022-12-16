e6a89faa691f813720bf7c2919f6a42c803aaa27
Kevin Marete <kevinmmarete@gmail.com>
2022-12-15 21:43:25 -0500

    Added hidden partner geo coordinates

--- resources/views/landing/pages/home.blade.php
+++ resources/views/landing/pages/home.blade.php
@@ -188,7 +188,8 @@
                 </div>
                 <div class="row">
                     <div class="col-md-12 mb-4">
-                        <div id="distribution_map" style="height: 180px;"></div>
+                        <input type="hidden" id="partner_geo_coordinates" data-coordinates="{{ $partner_geo_coordinates }}" />
+                        <div id="distribution_map" style="height: 300px;"></div>
                     </div>
                 </div>
             </div>

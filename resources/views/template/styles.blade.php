c5db9398e0afdace94b61b64cc90ed27c0dd50c7
Kevin Marete <kevinmmarete@gmail.com>
2023-01-29 18:05:39 -0500

    Added js-zip, pdfmake, buttons to styles template

--- resources/views/template/styles.blade.php
+++ resources/views/template/styles.blade.php
@@ -11,3 +11,4 @@
 <script src="/libs/ajax/libs/feather-icons/4.24.1/feather.min.js" crossorigin="anonymous"></script>
 <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&amp;display=swap" rel="stylesheet" />
 <link href="https://fonts.googleapis.com/css?family=Libre+Franklin:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;display=swap" rel="stylesheet" />
+<link rel="stylesheet" type="text/css" href="/libs/buttons/2.3.3/css/buttons.dataTables.css"/>
